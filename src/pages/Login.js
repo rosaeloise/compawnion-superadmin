88254ace93a5b4be67ebaf3d309211d578d6e1e3
Ely Rose Bosangit <bosangitelyrose05@gmail.com>
2024-12-28 05:00:21 +0800

    fix button css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,7 +7,7 @@ class Login extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			showPassword: false, // State to toggle password visibility
+			showPassword: false,
 		};
 	}
 
